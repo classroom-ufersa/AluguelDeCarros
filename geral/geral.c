131347fda2c0c8f4fbd27a66c2508ec26ec02da5
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2023-09-27 16:13:26 -0300

    att

---
+++ geral/geral.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cliente.h"
+#include "carro.h"
+#include "geral.h"
---
+++ geral/geral.h
@@ -0,0 +1,8 @@
+/* Dependência de módulo */
+#include "cliente.h"
+#include "carro.h"
+
+/* Funções Exportadas */
+
+/* Função
+*/
